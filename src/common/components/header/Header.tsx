9ef3c3a14a6963df37b7886a91907cb90f1da9d0
Aisylu24 <firnatovna@mail.ru>
2022-12-05 20:32:40 +0300

    feat(Header): add Navlink

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
+import {ROUTES} from '../../enums/enums'
+
 
 export const Header = () => {
     return (
-        <div>
-
-        </div>
+        <ul className={s.header}>
+            <li>
+                <NavLink to={ROUTES.MAIN} title={'Main'} className={s.link}>Main</NavLink>
+            </li>
+            <li>
+                <NavLink to={ROUTES.NEWS} title={'News'} className={s.link}>News</NavLink>
+            </li>
+        </ul>
     );
 };
 
--- src/common/components/routes/Pages.tsx
+++ src/common/components/routes/Pages.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {Main} from "../../../features/main/Main";
 import {News} from "../../../features/news/News";
 import {Routes, Route, Navigate} from 'react-router-dom'
+import s from "./Pages.module.css"
 
 export const Pages = () => {
     return (
+        <div className={s.pages}>
         <Routes>
             <Route path={'/'} element={<Main/>}/>
             <Route path={'news'} element={<News/>}/>
             <Route path='404' element={<h1>404: PAGE NOT FOUND</h1>}/>
             <Route path='/*' element={<Navigate to='404'/>}/>
         </Routes>
+        </div>
     );
 };
